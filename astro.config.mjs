ee65cc207313fe4883cfcaff420b2b1604f0ea06
Aimar <aimarmerodoa@gmail.com>
2026-02-24 12:27:17 +0100

    feat: Add new store product pages for Hytale, Rust, Minecraft, GTA5, and TeamSpeak3, implement a new MainLayout, and include robots.txt.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vhost.tech',
+  integrations: [sitemap()],
 
   // Optimizaciones de build
   build: {
